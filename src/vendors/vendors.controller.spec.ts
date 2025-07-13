d5d6fe10551a37ab417c80b158acbdf6fe57cb0e
Nkematu Bonaventure <74823094+ChuloCr8v@users.noreply.github.com>
2025-07-13 17:38:18 +0100

    testing

--- src/vendors/vendors.controller.spec.ts
+++ src/vendors/vendors.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { VendorsController } from './vendors.controller';
+import { VendorsController } from './vendors.controller.js';
 
 describe('VendorsController', () => {
   let controller: VendorsController;
